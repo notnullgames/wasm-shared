4650469c6f06a402658336bade02dad996131b96
David Konsumer <konsumer@jetboystudio.com>
2024-07-07 20:05:55 -0700

    simplify site with build on dev-machine

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-mkdir -p build
+mkdir -p docs
 
 cd host
 emcmake cmake -B build -GNinja
 cmake --build build
-mv build/host.* ../build
+mv build/host.* ../docs
 
 cd ../cart
 cmake -B build -GNinja
 cmake --build build
-mv build/cart.* ../build
+mv build/cart.* ../docs
